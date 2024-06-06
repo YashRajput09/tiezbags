9ce37acacb42189bd88a2358664fd16d18d33200
Yash Rajput <rajputyash8561@gmail.com>
2024-06-06 23:20:34 +0530

    show bgcolor, pannelcolor, textcolor according to bag

--- init/data.js
+++ init/data.js
@@ -5,13 +5,19 @@ const sampleData = [
             url: "https://outback.life/cdn/shop/products/1_7ae35be8-58ce-4f4b-9838-fd5ccfa542a3.png?v=1628667466&width=1800",
         },
         price: 1999,
+        bgcolor: "#d1a069",
+        panelcolor: "#ad804d",
+        textcolor: "#000000",
     },
     {
         title: "Backpack",
         image: {
-            url: "https://rukminim2.flixcart.com/image/612/612/xif0q/backpack/9/h/x/-original-imagz5cykpxavgxm.jpeg?q=70",
+            url: "https://m.media-amazon.com/images/I/41742CYNpJL._SY300_SX300_.jpg",
         },
         price: 529,
+        bgcolor: "#569bd8",
+        panelcolor: "#627cd3",
+        textcolor: "#000e42",
     },
     {
         title: "Paper Bag",
@@ -19,6 +25,9 @@ const sampleData = [
             url: "https://rukminim2.flixcart.com/image/612/612/xif0q/party-gift-bag/d/f/4/5-n-1705-paper-bags-party-bag-urban-infotech-printed-original-imagzxbf3fydfrhg.jpeg?q=70",
         },
         price: 249,
+        bgcolor: "#2e3d92",
+        panelcolor: "#1e3580",
+        textcolor: "#440036",
     },
     {
         title: "Pink Attack",
@@ -26,6 +35,9 @@ const sampleData = [
             url: "https://rukminim2.flixcart.com/image/612/612/xif0q/backpack/m/w/0/girls-latest-stylish-cool-design-school-casual-college-bag-original-imagf5dwegmnc8pu.jpeg?q=70",
         },
         price: 307,
+        bgcolor: "#922e8f",
+        panelcolor: "#801e7b",
+        textcolor: "#440036",
     },
     {
         title: "Surprise",
@@ -33,6 +45,9 @@ const sampleData = [
             url: "https://5.imimg.com/data5/SELLER/Default/2023/6/318158961/CC/YA/SI/84648758/shoe-rope-bag-1000x1000.jpg",
         },
         price: 99,
+        bgcolor: "#d1a069",
+        panelcolor: "#ad804d",
+        textcolor: "#000000",
     },
     {
         title: "Waterproof",
@@ -40,6 +55,9 @@ const sampleData = [
             url: "https://redhorns.in/cdn/shop/files/BP003A_1.jpg?v=1705499339&width=1800",
         },
         price: 1899,
+        bgcolor: "#46464c",
+        panelcolor: "#2f3137",
+        textcolor: "#000000",
     },
     {
         title: "Laptop Bag",
@@ -47,6 +65,9 @@ const sampleData = [
         url: "https://m.media-amazon.com/images/I/717Rp03xdmL._AC_SL1500_.jpg",
         },
         price: 2108,
+        bgcolor: "#46464c",
+        panelcolor: "#2f3137",
+        textcolor: "#000000",
     },
     {
         title: "Travel Bafg",
@@ -54,6 +75,9 @@ const sampleData = [
             url: "https://images-cdn.ubuy.co.in/64b87c62d1740b715557f56b-layover-travel-bag-stealth-black-22.jpg",
         },
         price: 21309,
+        bgcolor: "#46464c",
+        panelcolor: "#2f3137",
+        textcolor: "#000000",
     },
     
 ]
