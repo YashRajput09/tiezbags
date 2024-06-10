65ad6af1454a71355350927aea6296d42a118fe0
Yash Rajput <rajputyash8561@gmail.com>
2024-06-11 01:41:23 +0530

    create route for add product and add ui

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const productsController = require('../controllers/products.js')
 
 router.get('/',  productsController.index);
-router.get('/new', productsController.newProductForm)
+router.get('/newProduct', productsController.newProductForm)
 
 module.exports = router;
